6e68936cd76697a8943695e2050713aed8919b5c
KienLe0311 <letrungkien0311@gmail.com>
2025-07-09 23:01:41 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/vocabulary/MainActivity.java
+++ app/src/main/java/com/example/vocabulary/MainActivity.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -76,6 +77,14 @@ public class MainActivity extends AppCompatActivity {
 //            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
 //            startActivity(intent);
 //        });
+        LinearLayout settingLayout = findViewById(R.id.menuSettings); // ID bạn sẽ đặt trong XML
+        settingLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ItemSettingActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
